6a60a729495b5201e6b503b9fd046ca2a82cd5a8
Megan Coyle <meg@mcoyle.com>
2016-04-20 22:47:55 -0400

    Add handlebars.js and set up templating for API calls

--- js/app.js
+++ js/app.js
@@ -1,3 +1,133 @@
 /*
-  Please add all Javascript code to this file.
+  Feedr js
 */
+
+var $article = $("article");
+var $popUp = $("#popUp");
+var $search = $("#search");
+var $nav = $("nav");
+var $mashableValue = $("#mashable");
+var $redditValue = $("#reddit");
+var $diggValue = $("#digg");
+var $initialValue = $("#initial-value span");
+var $mainContent = $("#main");
+
+
+
+// On mouseover of specific article, show popup
+// $article.mouseover(function() {
+//   $popUp.show();
+//      })
+
+$redditValue.click(function() {
+  $mainContent.html("");
+  $initialValue.text("Reddit");
+  searchReddit();
+})
+
+// REDDIT API
+// data.children.data.title
+// data.children.data.url //URL to content, add to title link
+// data.children.data.thumnail //URL for image
+// data.children.data.url //URL to content, add read more link
+// data.children.data.subreddit //category
+// data.children.data.score
+
+function searchReddit() {
+  var url = 'https://www.reddit.com/top.json';
+  $.get(url, function(response){
+      for (i = 0; i < response.data.children.length; i++) {
+        var titleArticle = response.data.children[i].data.title;
+        var contentUrl = response.data.children[i].data.url;
+        var featureImage = response.data.children[i].data.thumbnail;
+        var category = response.data.children[i].data.subreddit;
+        var count = response.data.children[i].data.score;
+        var article = {
+          title: titleArticle,
+          impressions: count,
+          tag: category,
+          image: featureImage,
+          link: contentUrl
+        }
+        var source = $("#article-template").html();
+        var template = Handlebars.compile(source);
+        $("#main").append(template(article));
+      }
+  })
+}
+
+
+$mashableValue.click(function() {
+  $mainContent.html("");
+  $initialValue.text("Mashable");
+  searchMashable();
+})
+
+// MASHABLE API
+// new.title
+// new.link // link to full content
+// new.feature_image //URL for image
+// new.content
+// new.channel // category
+// new.shares.total
+
+function searchMashable() {
+  var url = 'http://feedr-api.wdidc.org/mashable.json';
+  $.get(url, function(response){
+      for (i = 0; i < response.new.length; i++) {
+        var titleArticle = response.new[i].title;
+        var contentUrl = response.new[i].link;
+        var featureImage = response.new[i].feature_image;
+        var category = response.new[i].channel;
+        var count = response.new[i].shares.total;
+        var article = {
+          title: titleArticle,
+          impressions: count,
+          tag: category,
+          image: featureImage,
+          link: contentUrl
+        }
+        var source = $("#article-template").html();
+        var template = Handlebars.compile(source);
+        $("#main").append(template(article));
+      }
+  })
+}
+
+$diggValue.click(function() {
+  $mainContent.html("");
+  $initialValue.text("Digg");
+  searchDigg();
+})
+
+// DIGG API
+// data.feed.content.title
+// data.feed.content.url // Link to full content
+// data.feed.content.media.images[0].url //URL for image
+// data.feed.content.description
+// data.feed.content.domain_name // category
+// data.feed.diggs.count
+
+function searchDigg() {
+  var url = 'http://feedr-api.wdidc.org/digg.json';
+  $.get(url, function(response){
+    // console.log(response);
+      for (i = 0; i < response.data.feed.length; i++) {
+        var titleArticle = response.data.feed[i].content.title;
+        var contentUrl = response.data.feed[i].content.url;
+        var featureImage = response.data.feed[i].content.media.images[0].url;
+        var category = response.data.feed[i].content.domain_name;
+        var count = response.data.feed[i].diggs.count;
+        var article = {
+          title: titleArticle,
+          impressions: count,
+          tag: category,
+          image: featureImage,
+          link: contentUrl
+        }
+        var source = $("#article-template").html();
+        var template = Handlebars.compile(source);
+        $("#main").append(template(article));
+      }
+  })
+}
